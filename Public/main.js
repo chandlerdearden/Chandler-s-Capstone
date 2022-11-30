f73d0e80cba1a7f755662c6e45e04cb30a424947
Chandler Dearden <chandlerjdearden@gmail.com>
2022-11-30 15:05:51 -0700

    updated meals page

--- Public/main.js
+++ Public/main.js
@@ -1,5 +1,6 @@
 const mealContainer = document.querySelector('#mealsContainer')
 const mealSelect = document.querySelector('#meal-selector')
+const addMealBtn = document.querySelector("#addMealBtn")
 
 
 
@@ -9,6 +10,7 @@ const mealsCallback = ({ data: meals }) => displayMeals(meals)
 
 const getAllMeals = () => axios.get(baseURL).then(mealsCallback)
 const deleteMeal = id => axios.delete(`${baseURL}/${id}`).then(mealsCallback)
+const createMeal = body => axios.post(baseURL, body).then(mealsCallback)
 
 
 function displayMeals(arr) {
@@ -43,7 +45,29 @@ function getMealOptions () {
     } )
 }
 
+function createMealHandler () {
+    let image = document.querySelector('#newMealImage')
+    let name = document.querySelector('#newMealName')
+    let ingredients = document.querySelector('#newMealIngredients')
+    let calories = document.querySelector('#newMealCalories')
+
+    body = {
+        name: name.value,
+        ingredients: ingredients.value,
+        calories: calories.value,
+        imageURL: image.value
+    }
+    createMeal(body)
+
+    name.value = ''
+    ingredients.value = ''
+    image.value = ''
+    calories.value = null
+ getAllMeals()
+}
+
 getMealOptions()
 getAllMeals()
+addMealBtn.addEventListener('click', createMealHandler)
 
 
