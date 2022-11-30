68ac30d922c67519fdb78e02537854d01caa4526
Chandler Dearden <chandlerjdearden@gmail.com>
2022-11-30 13:26:56 -0700

    Added Html get and delete

--- Public/main.js
+++ Public/main.js
@@ -1,1 +1,35 @@
-const baseURL = "http://localhost:4056"
+const mealContainer = document.querySelector('#mealsContainer')
+
+
+const baseURL = "http://localhost:4056/api/meals"
+
+const mealsCallback = ({ data: meals }) => displayMeals(meals)
+
+const getAllMeals = () => axios.get(baseURL).then(mealsCallback)
+const deleteMeal = id => axios.delete(`${baseURL}/${id}`).then(mealsCallback)
+
+
+function displayMeals(arr) {
+    mealContainer.innerHTML = ``
+    for (let i = 0; i < arr.length; i++) {
+        createMealCard(arr[i])
+    }
+}
+
+function createMealCard(meal) {
+    const mealCard = document.createElement('div')
+    
+    mealCard.innerHTML = `<img alt='meal image' src=${meal.image}/>
+    <h3>${meal.name}</h3>
+    <p>${meal.ingredients}</p>
+    <p>${meal.calories} Calories</p>
+    <button onclick="deleteMeal(${meal.id})">delete</button>
+    `
+
+
+    mealContainer.appendChild(mealCard)
+}
+
+getAllMeals()
+
+
