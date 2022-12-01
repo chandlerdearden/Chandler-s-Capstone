1d43ed20487f41cd49f863563dd006899ede9fcd
Chandler Dearden <chandlerjdearden@gmail.com>
2022-12-01 15:29:34 -0700

    made progress

--- Public/main.js
+++ Public/main.js
@@ -1,4 +1,15 @@
 const mealContainer = document.querySelector('#mealsContainer')
+const mealSelectMain =document.querySelector('#mealSelectMain')
+const addMealToScheduleBtn = document.querySelector("#addMealToScheduleBtn")
+const daySelect = document.querySelector("#daySelect")
+const sunday = document.querySelector("#sunday")
+const monday = document.querySelector("#monday")
+const tuesday = document.querySelector("#tuesday")
+const wednesday = document.querySelector("#wednesday")
+const thursday = document.querySelector("#thursday")
+const friday = document.querySelector("#friday")
+const saturday = document.querySelector("#saturday")
+
 
 const baseURL = "http://localhost:4056/api/meals"
 
@@ -10,7 +21,9 @@ const createMeal = body => axios.post(baseURL, body).then(mealsCallback)
 
 
 function displayMeals(arr) {
-    mealContainer.innerHTML = ``
+    mealContainer.innerHTML = `
+    <h1>Saved Meals</h1>
+            `
     for (let i = 0; i < arr.length; i++) {
         createMealCard(arr[i])
     }
@@ -29,8 +42,68 @@ function createMealCard(meal) {
 
     mealContainer.appendChild(mealCard)
 }
+function getMealOptions () {
+    axios.get(baseURL).then(res =>{
+        const meals = res.data
+        for(i=0; i <meals.length; i++) {
+            let option = document.createElement('option')
+            option.value = meals[i].name
+            option.textContent = meals[i].name
+            mealSelectMain.appendChild(option)
+        }
+    } )
+    
+}
+function addMealToSchedule () {
+    let day = daySelect.value
+    let meal = mealSelectMain.value
+    axios.get(baseURL)
+    .then(res => {
+         const meals = res.data
+        let index = meals.findIndex(meals => meals.name === meal)
+        let addedMeal = document.createElement('div')
+        addedMeal.innerHTML = `
+        <img alt='meal image' src=${meals[index].image}/>
+        <p>${meals[index].name}</p>
+        <p>${meals[index].calories}</p>
+        <button onclick = "" >Remove</button>
+        `
+        if(day === "sunday"){
+            sunday.appendChild(addedMeal)
+        } 
+        else if ( day === "monday") {
+            monday.appendChild(addedMeal)
+        }
+        else if ( day === "tuesday") {
+            tuesday.appendChild(addedMeal)
+        }
+        else if ( day === "wednesday") {
+            wednesday.appendChild(addedMeal)
+        }
+        else if ( day === "thursday") {
+            thursday.appendChild(addedMeal)
+        }
+        else if ( day === "friday") {
+            friday.appendChild(addedMeal)
+        } 
+        else if ( day === "saturday") {
+            saturday.appendChild(addedMeal)
+        } else (
+            alert('choose a day')
+        )
 
 
+
+    })
+
+
+}
+function deleteFromSchedule () {
+
+}
+
+addMealToScheduleBtn.addEventListener('click', addMealToSchedule)
+getMealOptions()
 getAllMeals()
 
 
